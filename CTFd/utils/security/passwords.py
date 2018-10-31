315cb5fac0895a9a96ff8e96a5c0439e3d75fb8f
Leon Chou <leon.chou@nyu.edu>
2018-10-31 16:55:56 -0400

    added plugin

--- CTFd/utils/security/passwords.py
+++ CTFd/utils/security/passwords.py
@@ -3,12 +3,12 @@ import hashlib
 
 
 def hash_password(p):
-    return bcrypt_sha256.encrypt(p)
+    return bcrypt_sha256.encrypt(p.encode('utf-8'))
 
 
 def check_password(p, hash):
-    return bcrypt_sha256.verify(p, hash)
+    return bcrypt_sha256.verify(p.encode('utf-8'), hash)
 
 
 def sha256(p):
-    return hashlib.sha256(p).hexdigest()
+    return hashlib.sha256(p.encode('utf-8')).hexdigest()
