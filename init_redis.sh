458ca1a57d19e5d4c5709fb2358cf5a95263ad99
Leon Chou <leon.chou@nyu.edu>
2018-10-31 20:52:01 -0400

    more redis

--- init_redis.sh
+++ init_redis.sh
@@ -5,5 +5,5 @@ sudo cp ./redis.conf /etc/redis/
 
 sudo adduser --system --group --no-create-home redis
 sudo mkdir /var/lib/redis
-sudo chown redis:redis /var/lib/redis
-sudo chmod 770 /var/lib/redis
+sudo chown redis:redis -R /var/lib/redis
+sudo chmod 770 -R /var/lib/redis
